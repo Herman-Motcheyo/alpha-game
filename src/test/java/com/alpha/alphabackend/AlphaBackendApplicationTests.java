5b3a2158f7cdfffd0c2a35be1e495e8798c098cf
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2021-05-31 05:14:38 +0100

    initial commit

---
+++ src/test/java/com/alpha/alphabackend/AlphaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.alphabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
